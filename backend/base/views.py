a052b67cccf533fc5fe82dcd303312031017b04d
fedotovdmitriy14 <fedotovdmitriy14@gmail>
2021-11-04 19:51:27 +0300

    add frontend and backend

---
+++ backend/base/views.py
@@ -0,0 +1,29 @@
+from django.http import JsonResponse
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import *
+from .products import products
+from .serializers import ProductSerializer
+
+
+@api_view(['GET'])
+def getRoutes(request):
+    return Response("Hello")
+
+
+@api_view(['GET'])
+def getProducts(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getProduct(request, pk):
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
+
+
